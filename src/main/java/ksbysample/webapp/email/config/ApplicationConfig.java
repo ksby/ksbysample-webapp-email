add9ef2ce692e9b551930b8608bb6dbeca68ac9e
ksby <kusa@db3.so-net.ne.jp>
2015-04-12 11:25:57 +0900

    Initial commit

---
+++ src/main/java/ksbysample/webapp/email/config/ApplicationConfig.java
@@ -0,0 +1,21 @@
+package ksbysample.webapp.email.config;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.jpa.HibernateEntityManagerFactory;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class ApplicationConfig {
+
+    @Bean
+    @ConfigurationProperties("spring.datasource")
+    public DataSource dataSource() {
+        return DataSourceBuilder.create().build();
+    }
+
+}
